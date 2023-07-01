f3caca5462facf21b6ffdf622468d47c8326b97d
rehman <mrwolverine42034@gmail.com>
2023-07-01 16:18:17 +0500

    Footer modify

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,40 +1,42 @@
-import React from 'react'
+import React from "react";
+import { BsFacebook } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
 
 export default function Footer() {
   return (
     <footer class="bg-dark text-center text-white">
-  <div class="container p-4 pb-0">
-    <section class="mb-4">
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-        ><i class="fab fa-facebook-f"></i
-      ></a>
+      <div class="container p-4 pb-0">
+        <section class="mb-4">
+          <a
+            href={"https://www.facebook.com/thepchighschool/"}
+            class="btn btn-outline-light btn-floating m-1"
+            target="_blank"
+            role="button"
+          >
+            <BsFacebook />
+          </a>
+          <a
+            class="btn btn-outline-light btn-floating m-1"
+            href={
+              "https://www.instagram.com/thepchighschooltphs/"
+            }
+            role="button"
+            target="_blank"
+          >
+            <BsInstagram />
+          </a>
+        </section>
+      </div>
 
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-        ><i class="fab fa-twitter"></i
-      ></a>
-
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-        ><i class="fab fa-google"></i
-      ></a>
-
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-        ><i class="fab fa-instagram"></i
-      ></a>
-
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-        ><i class="fab fa-linkedin-in"></i
-      ></a>
-
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-        ><i class="fab fa-github"></i
-      ></a>
-    </section>
-  </div>
-
-  <div class="text-center p-3" style={{backgroundColor:`rgba(${0}, ${0}, ${0}, ${0.2})`}}>
-    © 2020 Copyright:
-    <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-  </div>
-</footer>
-  )
+      <div
+        class="text-center p-3"
+        style={{ backgroundColor: `rgba(${0}, ${0}, ${0}, ${0.2})` }}
+      >
+        © 2020 Copyright:
+        <a class="text-white" href="https://mdbootstrap.com/">
+          MDBootstrap.com
+        </a>
+      </div>
+    </footer>
+  );
 }
